9b9e2836873a900059d0caef5c4248af040278a4
haeun <praise1109@naver.com>
2022-11-01 12:37:39 +0900

    20221101

--- src/components/section/Card.js
+++ src/components/section/Card.js
@@ -1,90 +1,81 @@
-import React from 'react'
+import React from "react";
 
-function Card({attr}) {
-  return (
-    <section id="cardType" className={`card__wrap ${attr[0]} ${attr[1]}`}>
-    <h2>애니멀 활동 소개 📝</h2>
-    <p>애니멀 유치원의 대표적인 활동 리스트 입니다.</p>
-    <div className={`card__inner ${attr[2]}`}>
-      <article className="card">
-        <figure className="card__header">
-          <img src="../assets/img/card_bg01_01.jpg" alt="상업 전시" />
-        </figure>
-        <div className="card__body">
-          <h3 className="tit">개인 활동 시간</h3>
-          <p className="desc">
-            당신의 각각의 반려동물의 맞춤 교육 활동입니다. 각 애니멀들의 성향 및 고칠점에 대하여 전문가들과 분석하고 1:1 맞춤형으로 교육이 진행됩니다.
-          </p>
-          <a className="btn" href="/">
-            더 자세히 보기
-            <span aria-hidden="true">
-              <svg
-                width="65"
-                height="8"
-                viewBox="0 0 65 8"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M64.3536 4.35355C64.5488 4.15829 64.5488 3.84171 64.3536 3.64645L61.1716 0.464466C60.9763 0.269204 60.6597 0.269204 60.4645 0.464466C60.2692 0.659728 60.2692 0.976311 60.4645 1.17157L63.2929 4L60.4645 6.82843C60.2692 7.02369 60.2692 7.34027 60.4645 7.53553C60.6597 7.7308 60.9763 7.7308 61.1716 7.53553L64.3536 4.35355ZM0 4.5H64V3.5H0V4.5Z"
-                  fill="black"
-                />
-              </svg>
-            </span>
-          </a>
-        </div>
-      </article>
-      <article className="card">
-        <figure className="card__header">
-          <img src="../assets/img/card_bg01_02.jpg" alt="미술 전시" />
-        </figure>
-        <div className="card__body">
-          <h3 className="tit">간식 시간</h3>
-          <p className="desc">
-            식사 훈련을 함께 동반하며 사료와 함께 수제로 만든 건강 간식이 제공됩니다. 필요에 따라 직접 선택할 수 있으며 주의 사항에 따라 해당 재료를 먹지 못하는 경우 따로 제공합니다.
-          </p>
-          <a className="btn" href="/">
-            더 자세히 보기
-            <span aria-hidden="true">
-              <svg
-                width="65"
-                height="8"
-                viewBox="0 0 65 8"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M64.3536 4.35355C64.5488 4.15829 64.5488 3.84171 64.3536 3.64645L61.1716 0.464466C60.9763 0.269204 60.6597 0.269204 60.4645 0.464466C60.2692 0.659728 60.2692 0.976311 60.4645 1.17157L63.2929 4L60.4645 6.82843C60.2692 7.02369 60.2692 7.34027 60.4645 7.53553C60.6597 7.7308 60.9763 7.7308 61.1716 7.53553L64.3536 4.35355ZM0 4.5H64V3.5H0V4.5Z"
-                  fill="black"
-                />
-              </svg>
-            </span>
-          </a>
-        </div>
-      </article>
-      <article className="card">
-        <figure className="card__header">
-          <img src="../assets/img/card_bg01_03.jpg" alt="사진 전시" />
-        </figure>
-        <div className="card__body">
-          <h3 className="tit">단체 활동 시간</h3>
-          <p className="desc">
-            개인 활동과 달리 단체로 활동하는 시간이며 각 애니멀들의 사회성, 사교성을 기르기 위한 교육 시간 입니다. 각 성향에 따라 분리 하여 활동할 수 있습니다.
-          </p>
-          <a className="btn" href="/">
-            더 자세히 보기
-            <span aria-hidden="true">
-              <svg width="65" height="8" viewBox="0 0 65 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M64.3536 4.35355C64.5488 4.15829 64.5488 3.84171 64.3536 3.64645L61.1716 0.464466C60.9763 0.269204 60.6597 0.269204 60.4645 0.464466C60.2692 0.659728 60.2692 0.976311 60.4645 1.17157L63.2929 4L60.4645 6.82843C60.2692 7.02369 60.2692 7.34027 60.4645 7.53553C60.6597 7.7308 60.9763 7.7308 61.1716 7.53553L64.3536 4.35355ZM0 4.5H64V3.5H0V4.5Z" fill="black" />
-              </svg>
-            </span>
-          </a>
-        </div>
-      </article>
-    </div>
-  </section>
-
-  )
+function Card({ attr }) {
+    return (
+        <section id="cardType" className={`card__wrap ${attr[0]} ${attr[1]}`}>
+            <h2>애니멀 활동 소개 📝</h2>
+            <p>애니멀 유치원의 대표적인 활동 리스트 입니다.</p>
+            <div className={`card__inner ${attr[2]}`}>
+                <article className="card">
+                    <figure className="card__header">
+                        <img src="../assets/img/card_bg01_01.jpg" alt="상업 전시" />
+                    </figure>
+                    <div className="card__body">
+                        <h3 className="tit">개인 활동 시간</h3>
+                        <p className="desc">
+                            당신의 각각의 반려동물의 맞춤 교육 활동입니다. 각
+                            애니멀들의 성향 및 고칠점에 대하여 전문가들과
+                            분석하고 1:1 맞춤형으로 교육이 진행됩니다.
+                        </p>
+                        <a className="btn" href="/">
+                            더 자세히 보기
+                            <span aria-hidden="true">
+                                <svg width="65" height="8" viewBox="0 0 65 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M64.3536 4.35355C64.5488 4.15829 64.5488 3.84171 64.3536 3.64645L61.1716 0.464466C60.9763 0.269204 60.6597 0.269204 60.4645 0.464466C60.2692 0.659728 60.2692 0.976311 60.4645 1.17157L63.2929 4L60.4645 6.82843C60.2692 7.02369 60.2692 7.34027 60.4645 7.53553C60.6597 7.7308 60.9763 7.7308 61.1716 7.53553L64.3536 4.35355ZM0 4.5H64V3.5H0V4.5Z" fill="black" />
+                                </svg>
+                            </span>
+                        </a>
+                    </div>
+                </article>
+                <article className="card">
+                    <figure className="card__header">
+                        <img
+                            src="../assets/img/card_bg01_02.jpg"
+                            alt="미술 전시"
+                        />
+                    </figure>
+                    <div className="card__body">
+                        <h3 className="tit">간식 시간</h3>
+                        <p className="desc">
+                            식사 훈련을 함께 동반하며 사료와 함께 수제로 만든
+                            건강 간식이 제공됩니다. 필요에 따라 직접 선택할 수
+                            있으며 주의 사항에 따라 해당 재료를 먹지 못하는 경우
+                            따로 제공합니다.
+                        </p>
+                        <a className="btn" href="/">
+                            더 자세히 보기
+                            <span aria-hidden="true">
+                                <svg width="65" height="8" viewBox="0 0 65 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M64.3536 4.35355C64.5488 4.15829 64.5488 3.84171 64.3536 3.64645L61.1716 0.464466C60.9763 0.269204 60.6597 0.269204 60.4645 0.464466C60.2692 0.659728 60.2692 0.976311 60.4645 1.17157L63.2929 4L60.4645 6.82843C60.2692 7.02369 60.2692 7.34027 60.4645 7.53553C60.6597 7.7308 60.9763 7.7308 61.1716 7.53553L64.3536 4.35355ZM0 4.5H64V3.5H0V4.5Z" fill="black" />
+                                </svg>
+                            </span>
+                        </a>
+                    </div>
+                </article>
+                <article className="card">
+                    <figure className="card__header">
+                        <img src="../assets/img/card_bg01_03.jpg" alt="사진 전시" />
+                    </figure>
+                    <div className="card__body">
+                        <h3 className="tit">단체 활동 시간</h3>
+                        <p className="desc">
+                            개인 활동과 달리 단체로 활동하는 시간이며 각
+                            애니멀들의 사회성, 사교성을 기르기 위한 교육 시간
+                            입니다. 각 성향에 따라 분리 하여 활동할 수 있습니다.
+                        </p>
+                        <a className="btn" href="/">
+                            더 자세히 보기
+                            <span aria-hidden="true">
+                                <svg width="65" height="8" viewBox="0 0 65 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M64.3536 4.35355C64.5488 4.15829 64.5488 3.84171 64.3536 3.64645L61.1716 0.464466C60.9763 0.269204 60.6597 0.269204 60.4645 0.464466C60.2692 0.659728 60.2692 0.976311 60.4645 1.17157L63.2929 4L60.4645 6.82843C60.2692 7.02369 60.2692 7.34027 60.4645 7.53553C60.6597 7.7308 60.9763 7.7308 61.1716 7.53553L64.3536 4.35355ZM0 4.5H64V3.5H0V4.5Z" fill="black" />
+                                </svg>
+                            </span>
+                        </a>
+                    </div>
+                </article>
+            </div>
+        </section>
+    );
 }
 
-export default Card
+export default Card;
--- src/components/section/Image.js
+++ src/components/section/Image.js
@@ -1,12 +1,47 @@
-import React from 'react'
+import React from 'react';
+
+const imageTextTitle = {
+    hadding: "애니멀 유치원",
+    desc: "🐶 당신의 반려 동물에 맞는 반을 골라주세요!🐱"
+};
+
+const imageTextInfo = [
+    { 
+        title: "멍멍 유치원",
+        desc: "강아지는 매우 귀엽습니다. 활동적인 모습이 귀엽습니다. 강아지는 기뻐할 때도 귀엽고 시무룩할 때도 귀엽습니다.",
+        className: "img1",
+    },
+    {
+        title: "야옹 유치원",
+        desc: "고양이는 매우 귀엽습니다. 도도하면서도 애교가 많습니다. 고양이는 은근하게 다가오는 것이 매력적입니다.",
+        className: "img2",
+    }
+];
+
+const ImageText = ({title, desc, className}) => {
+    return (
+        // className을 넣으려면 props처럼 넣어주면 된다. 이때 앞에 배열이름은 안넣어도 되고 반복되는 데이터의 키 값만 넣으면 된다.
+        <article className={`image ${className}`}>
+            <h3 className="image__title">{title}</h3>
+            <p className="image__desc">{desc}</p>
+            <a href="/" title="자세히 보기" className="image__btn">자세히 보기</a>
+        </article>
+    );
+};
 
 function Image(props) {
   return (
     <section id="imageType" className={`image__wrap scroll ${props.attr[0]} ${props.attr[1]}`}>
-        <h2>애니멀 유치원</h2>
-        <p>🐶 당신의 반려 동물에 맞는 반을 골라주세요!🐱</p>
+        {/* <h2>애니멀 유치원</h2>
+        <p>🐶 당신의 반려 동물에 맞는 반을 골라주세요!🐱</p> */}
+        <h2>{imageTextTitle.hadding}</h2>
+        <p>{imageTextTitle.desc}</p>
         <div class={`image__inner ${props.attr[2]}`}>
-            <article className="image img1">
+            {imageTextInfo.map((info) => (
+                <ImageText  title={info.title}  desc={info.desc}  className={info.className}  />
+            ))}
+            {/* 아래의 반복되는 데이터를 컴퍼넌트 시켜서 데이터 관리와 수정을 효율적으로 할 수 있게 된다. */}
+            {/* <article className="image img1">
                 <h3 className="image__title">멍멍 유치원</h3>
                 <p className="image__desc">
                     강아지는 매우 귀엽습니다. 활동적인 모습이 귀엽습니다. 강아지는
@@ -21,7 +56,7 @@ function Image(props) {
                     은근하게 다가오는 것이 매력적입니다.
                 </p>
                 <a href="/" title="자세히 보기" className="image__btn yellow">자세히 보기</a>
-            </article>
+            </article> */}
         </div>
     </section>
 
